e03bf23bc7fa2f39eb3407b56ca47ca9f670fb92
Alev Ayaz <73062283+al3v@users.noreply.github.com>
2022-05-29 21:38:38 +0300

    Add files via upload
    Size gönderilen Google Drive linkinde, dosyalar/sinav.dat isimli bir dosya göreceksiniz. Bu
    dosya, test sınavlarında öğrencilerin cevaplarının okunduğu optik formlardan elde edilmiş bir optik
    okuyucu çıktısı. Bu arada belirteyim dosyada belirtilen öğrenci numaraları ve cevaplar gerçek değil.
    Her satırda bir öğrencinin belirli bir dersin sınavında optik forma kodladığı bilgiler ve cevaplar
    bulunuyor. Eğer öğrenci birden fazla dersin sınavına girdiyse, o öğrenciye ait birden fazla satır olabilir.
    Dosyadaki her satırın formatı şöyledir:
     1‐10 ogrenci numarasi
     11‐16 : salon no
     17‐19 : ders kodu
     20 : sınav grubu A B C D
     22 ‐ : yanıtlar (A,B,C,D,E seçenekleri ve boş bırakıldıysa boşluk karakteri)
    
    GÖREV 1: Sınava kaç tane farklı öğrencinin girdiğini sayacak bir BASH betiği yazacaksınız. Her öğrenci
    farklı sayıda derse girdiğinden, aynı öğrenci numarası birden fazla satırda yer alabilir. Bunu bir şekilde
    çözerek (ilişkisel dizilerle mesela), bir öğrenciyi sadece bir kere sayarak, toplam sayıyı bulmanızı
    istiyorum. while döngüsü, string işlemleri ve ilişkisel ağlar kullanılabilir. Programınız en fazla 20 satırı
    geçmemeli ve sonuç olarak 9127 rakamını bulmalısınız.

---
+++ gorev1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash  
+
+myarr=($(awk '{print $1}' sinav.dat )) 
+
+len=${#myarr[@]}
+
+for i in $(seq 0 $len); do
+	test+=($(echo ${myarr[i]:0:10})); 
+done
+
+#echo ${test[@]}
+
+
+uniqs_arr=($(for i in "${test[@]}"; do echo "${i}"; done | sort -u))
+
+lenn=${#uniqs_arr[@]}
+echo "Sinava $lenn farkli ogrenci girmistir"
+
+	
