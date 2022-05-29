161efe54e2f4cca85078d151392d5b834df70b2e
Alev Ayaz <73062283+al3v@users.noreply.github.com>
2022-05-29 21:45:14 +0300

    Add files via upload

---
+++ gorev3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash  
+
+myarr=($(awk '{print $1}' sinav.dat )) #getting the first column only
+
+len=${#myarr[@]}
+
+for i in $(seq 0 $len); do
+	test+=($(echo ${myarr[i]: -4:3 })); 
+done
+
+#echo ${test[@]}  #for debug purposes
+
+COUNTER=0
+for i in "${test[@]}"
+do
+    if [ "$i" -eq "$1" ] ; then
+#        echo "Found"
+        let COUNTER=COUNTER+1
+       
+    fi
+done
+ echo "Bu ders kodu $COUNTER tane satirda geciyor"
+
+
+
+	
