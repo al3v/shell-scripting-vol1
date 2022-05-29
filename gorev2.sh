d9e540f83b7d0bc381387f00536a59fdcb4570dd
Alev Ayaz <73062283+al3v@users.noreply.github.com>
2022-05-29 21:45:00 +0300

    Add files via upload

---
+++ gorev2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash  
+
+myarr=($(awk '{print $1}' sinav.dat )) #getting the first column only
+
+len=${#myarr[@]}
+
+for i in $(seq 0 $len); do
+	test+=($(echo ${myarr[i]: -4:3 })); 
+done
+
+#echo ${test[@]}  #for debug purposes
+
+
+uniqs_arr=($(for i in "${test[@]}"; do echo "${i}"; done | sort -u))
+
+lenn=${#uniqs_arr[@]}
+echo "Bu dosyada $lenn farkli ders kodu vardir"
+
+	
